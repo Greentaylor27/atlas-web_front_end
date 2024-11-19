78eebd230cd1bad8ba7292947e64540afb811e01
Greentaylor27 <greentaylor18@gmail.com>
2024-11-19 16:00:00 -0600

    I think this is what it is asking for.

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,33 @@
+function countPrimeNumbers() {
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+        let isPrime = true;
+        for (let x = 2; x <= Math.sqrt(i); x++){
+            if (i % x === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function countPrimeNumbers100Times() {
+    for (let count = 1; count <= 100; count++) {
+        countPrimeNumbers();
+    }
+}
+
+
+let sTime = performance.now();
+setTimeout(() => {
+    for (let i = 1; i <= 100; i++){
+        countPrimeNumbers100Times();
+    }
+    let eTime = performance.now();
+    let time = eTime - sTime;
+    console.log(`Execution time of calculating prime numbers 100 times was ${time} milliseconds.`);
+}, 0);
