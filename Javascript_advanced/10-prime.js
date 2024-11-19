8d4792ce2f9678336e7820b56528f88f60b7221d
Greentaylor27 <greentaylor18@gmail.com>
2024-11-19 15:39:55 -0600

    This seems right?

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,29 @@
+function countPrimeNumbers() {
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+        let isPrime = true;
+        for (let x = 2; x <= Math.sqrt(i); x++){
+            if (i % x === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function countPrimeNumbers100Times() {
+    for (let count = 1; count <= 100; count++) {
+        console.log(tracker)
+    }
+}
+
+let sTime = performance.now();
+countPrimeNumbers100Times();
+let eTime = performance.now();
+let time = eTime - sTime;
+
+console.log(`Execution time of counting countPrimeNumbers 100 times was ${time} milliseconds.`);
